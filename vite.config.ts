832939d57d38d6797352ddebe034e5072ab68fab
Kavish Khanna <your-actual-email@example.com>
2026-04-27 22:03:07 +0530

    Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   server: {
     port: 5173,
-    hmr: {
-      protocol: 'ws',
-      host: 'localhost',
-      port: 5173,
-    },
   },
 })
